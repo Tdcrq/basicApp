1ea4318ffe645d6168b69c39f4bfe0020533a58b
Tdcrq <90860668+Tdcrq@users.noreply.github.com>
2026-01-19 16:28:09 +0100

    core: init project

---
+++ app/index.tsx
@@ -0,0 +1,15 @@
+import { StyleSheet, Text, View } from "react-native";
+import {Link} from "expo-router";
+
+export default function Index() {
+  return (
+    <View style={styles.container}>
+        <Text>Edit app/index.tsx to edit this screen.</Text>
+        <Link href="/about">A propos</Link>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+    container: {backgroundColor: '#99adf8', flex: 1, padding: 24}
+})
---
+++ fiche/[id].tsx
@@ -0,0 +1,12 @@
+import {StyleSheet, Text, View} from "react-native";
+
+export default function Fiche() {
+    return <View style={styles.container}>
+        <Text></Text>
+    </View>
+}
+
+const styles = StyleSheet.create({
+    container: {backgroundColor: "#FFF000"},
+    main: {backgroundColor: "#99adf8"}
+})
